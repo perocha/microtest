8d3584ceb4239f93808eec384e173861e18d78ae
Pedro Rocha <pedrojrocha@outlook.com>
2024-03-29 21:43:05 +0100

    Refactor to remove unused code

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -7,60 +7,6 @@ import (
 	"github.com/microtest/telemetry"
 )
 
-/*
-// Method consumeMessages subscribes to the event hub and consumes messages
-func consumeMessages() {
-	// Subscribe to the event hub
-	err := messaging.EventHubInstance.Subscribe("Consumer subscribe", func(msg messaging.Message) {
-		// Log the event to App Insights
-		telemetry.TrackTrace("Consumer::Message received from EventHub", telemetry.Information, map[string]string{"payload": msg.Payload, "messageId": msg.MessageId})
-	})
-
-	// Check if there was an error subscribing to the event hub
-	if err != nil {
-		telemetry.TrackTrace("Consumer::Failed to subscribe to EventHub", telemetry.Error, map[string]string{"Error": err.Error()})
-	}
-}
-*/
-
-/*
-// New consume method that gets one message from eventhub and returns, so we can do something with the message
-
-	func consumeMessage(partitionID string) {
-		// Get one message from the event hub
-		msg, err := messaging.EventHubInstance.GetMessage("Consumer get message", partitionID)
-		if err != nil {
-			telemetry.TrackTrace("Consumer::Failed to get message from EventHub", telemetry.Error, map[string]string{"Error": err.Error()})
-			return
-		}
-
-		// Log the event to App Insights
-		telemetry.TrackTrace("Consumer::Message received from EventHub", telemetry.Information, map[string]string{"payload": msg.Payload, "messageId": msg.MessageId})
-	}
-
-	func main() {
-		// Initialize telemetry
-		telemetry.InitTelemetry("Consumer")
-
-		// Initialize a new EventHub instance
-		eventHubConnectionString := os.Getenv("EVENTHUB_CONSUMER_CONNECTION_STRING")
-		err := messaging.NewEventHub("Consumer init", eventHubConnectionString)
-		if err != nil {
-			telemetry.TrackTrace("Consumer::Failed to initialize EventHub", telemetry.Error, map[string]string{"Error": err.Error()})
-		}
-
-		// Keep the service running
-		for {
-			// Start consuming messages
-			//	consumeMessages()
-			partitionID := "0"
-			consumeMessage(partitionID)
-
-			time.Sleep(time.Second)
-		}
-	}
-*/
-
 func main() {
 	// Initialize telemetry
 	telemetry.InitTelemetry("Consumer")
