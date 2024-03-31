1ec3b92bf612da89068b36f6c633b377bdfacbc1
Pedro Rocha <pedrojrocha@outlook.com>
2024-03-31 16:06:03 +0200

    Updated consumer vnext

--- consumervnext/consumervnext.go
+++ consumervnext/consumervnext.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"os/signal"
+	"syscall"
 
 	"github.com/microtest/telemetry"
 
@@ -15,8 +17,10 @@ import (
 
 const (
 	SERVICE_NAME = "Consumer"
+	MaxPartitions = 4
 )
 
+/*
 // Shows how to start consuming events in partitions in an Event Hub using the [ConsumerClient].
 //
 // If you have an Azure Storage account you can use the [Processor] type instead, which will handle
@@ -65,7 +69,7 @@ func main() {
 
 	// Will wait up to 1 minute for 100 events. If the context is cancelled (or expires)
 	// you'll get any events that have been collected up to that point.
-	fmt.Println("Receiving events...")
+	fmt.Println("Consumervnext::Receiving events...")
 	receiveCtx, cancel := context.WithTimeout(context.TODO(), time.Minute)
 	events, err := partitionClient.ReceiveEvents(receiveCtx, 100, nil)
 	cancel()
@@ -86,4 +90,65 @@ func main() {
 	telemetry.TrackTrace("Done receiving events", telemetry.Information, nil)
 	fmt.Println("Consumervnext::Done receiving events")
 
+}
+
+*/
+
+func main() {
+	eventHubConnectionString := os.Getenv("EVENTHUB_CONNECTION_STRING")
+	eventHubName := os.Getenv("EVENTHUB_NAME")
+
+	// Initialize telemetry
+	telemetry.InitTelemetry(SERVICE_NAME)
+
+	eventHubConnectionString := os.Getenv("EVENTHUB_CONSUMERVNEXT_CONNECTION_STRING")
+	//	eventHubName := os.Getenv("EVENTHUB_NAME")
+	//	partitionID := os.Getenv("EVENTHUB_PARTITION_ID")
+	eventHubName := "microtest-eventhub2"
+	partitionID := "0"
+
+	fmt.Println("Consumervnext::EventHubName::", eventHubName)
+	fmt.Println("Consumervnext::PartitionID::", partitionID)
+	fmt.Println("Consumervnext::EventHubConnectionString::", eventHubConnectionString)
+
+	// Create new consumer client using connection string
+	consumerClients := make([]*azeventhubs.ConsumerClient, 0)
+
+	for i := 0; i < MaxPartitions; i++ {
+		consumerClient, err := createConsumerClient(eventHubConnectionString, eventHubName, i)
+		if err != nil {
+			fmt.Println("Consumervnext::Error creating consumer client for partition", i, ":", err)
+			telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Failed to create new event hub instance"})
+			return
+		}
+		defer consumerClient.Close(context.Background())
+
+		consumerClients = append(consumerClients, consumerClient)
+	}
+
+	// Run your processing logic here using consumerClients...
+
+	// Handle termination signals for graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	fmt.Println("Consumervnext::Shutting down...")
+	telemetry.TrackTrace("Consumervnext::Shutting down", telemetry.Information, nil)
+
+	// Close all consumer clients
+	for _, client := range consumerClients {
+		client.Close(context.Background())
+	}
+
+	fmt.Println("Consumervnext::Shutdown complete.")
+}
+
+func createConsumerClient(eventHubConnectionString, eventHubName string, partitionID int) (*azeventhubs.ConsumerClient, error) {
+	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, fmt.Sprintf("%d", partitionID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return consumerClient, nil
 }
