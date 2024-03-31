588cf70bda4a4cccd9c2bbd37db38f2161b20e1c
Pedro Rocha <pedrojrocha@outlook.com>
2024-03-31 21:00:25 +0200

    New version of consumer vnext

--- consumervnext/consumervnext.go
+++ consumervnext/consumervnext.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
-	"os/signal"
-	"strconv"
-	"syscall"
+	"time"
 
 	"github.com/microtest/telemetry"
 
@@ -18,14 +17,6 @@ const (
 	MaxPartitions = 4
 )
 
-/*
-// Shows how to start consuming events in partitions in an Event Hub using the [ConsumerClient].
-//
-// If you have an Azure Storage account you can use the [Processor] type instead, which will handle
-// distributing partitions between multiple consumers and storing progress using checkpoints.
-// See [example_consuming_with_checkpoints_test.go] for an example.
-//
-// [example_consuming_with_checkpoints_test.go]: https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/messaging/azeventhubs/example_consuming_with_checkpoints_test.go
 func main() {
 	// Initialize telemetry
 	telemetry.InitTelemetry(SERVICE_NAME)
@@ -40,116 +31,137 @@ func main() {
 	fmt.Println("Consumervnext::PartitionID::", partitionID)
 	fmt.Println("Consumervnext::EventHubConnectionString::", eventHubConnectionString)
 
-	// Create new consumer client using connection string
-	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
+	consumerClient, err := createClients(eventHubConnectionString, eventHubName)
 
 	if err != nil {
-		telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Failed to create new event hub instance"})
-		fmt.Println("Consumervnext::Failed to create new consumer client::" + err.Error())
+		fmt.Println("Error creating consumer client::", err)
 		panic(err)
 	}
 
 	defer consumerClient.Close(context.TODO())
 
-	partitionClient, err := consumerClient.NewPartitionClient(partitionID, &azeventhubs.PartitionClientOptions{
-		StartPosition: azeventhubs.StartPosition{
-			Earliest: to.Ptr(true),
-		},
-	})
+	// Create the Processor
+	//
+	// The Processor handles load balancing with other Processor instances, running in separate
+	// processes or even on separate machines. Each one will use the checkpointStore to coordinate
+	// state and ownership, dynamically.
+	fmt.Println("Creating processor")
+	processor, err := azeventhubs.NewProcessor(consumerClient, nil, nil)
 
 	if err != nil {
-		telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Failed to create new partition client"})
-		fmt.Println("Consumervnext::Failed to create new partition client::" + err.Error())
+		fmt.Println("Error creating processor::", err)
 		panic(err)
 	}
 
-	defer partitionClient.Close(context.TODO())
+	// Run in the background, launching goroutines to process each partition
+	go dispatchPartitionClients(processor)
 
-	// Will wait up to 1 minute for 100 events. If the context is cancelled (or expires)
-	// you'll get any events that have been collected up to that point.
-	fmt.Println("Consumervnext::Receiving events...")
-	receiveCtx, cancel := context.WithTimeout(context.TODO(), time.Minute)
-	events, err := partitionClient.ReceiveEvents(receiveCtx, 100, nil)
-	cancel()
+	// Run the load balancer. The dispatchPartitionClients goroutine (launched above)
+	// will receive and dispatch ProcessorPartitionClients as partitions are claimed.
+	//
+	// Stopping the processor is as simple as canceling the context that you passed
+	// in to Run.
+	processorCtx, processorCancel := context.WithCancel(context.TODO())
+	defer processorCancel()
 
-	if err != nil && !errors.Is(err, context.DeadlineExceeded) {
-		telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Error receiving events"})
-		fmt.Println("Consumervnext::Error receiving events::", err)
+	if err := processor.Run(processorCtx); err != nil {
+		fmt.Println("Error", err)
 		panic(err)
 	}
-
-	for _, event := range events {
-		// We're assuming the Body is a byte-encoded string. EventData.Body supports any payload
-		// that can be encoded to []byte.
-		telemetry.TrackTrace("Event received with body", telemetry.Information, map[string]string{"body": string(event.Body)})
-		fmt.Println("Consumervnext::Event received with body::", string(event.Body))
-	}
-
-	telemetry.TrackTrace("Done receiving events", telemetry.Information, nil)
-	fmt.Println("Consumervnext::Done receiving events")
-
 }
 
-*/
+func createClients(eventHubConnectionString, eventHubName string) (*azeventhubs.ConsumerClient, error) {
 
-func main() {
-	// Initialize telemetry
-	telemetry.InitTelemetry(SERVICE_NAME)
+	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
 
-	eventHubConnectionString := os.Getenv("EVENTHUB_CONSUMERVNEXT_CONNECTION_STRING")
-	//	eventHubName := os.Getenv("EVENTHUB_NAME")
-	//	partitionID := os.Getenv("EVENTHUB_PARTITION_ID")
-	eventHubName := "microtest-eventhub2"
-	partitionID := "0"
-
-	fmt.Println("Consumervnext::EventHubName::", eventHubName)
-	fmt.Println("Consumervnext::PartitionID::", partitionID)
-	fmt.Println("Consumervnext::EventHubConnectionString::", eventHubConnectionString)
-
-	// Create new consumer client using connection string
-	consumerClients := make([]*azeventhubs.ConsumerClient, 0)
-
-	for i := 0; i < MaxPartitions; i++ {
-		consumerClient, err := createConsumerClient(eventHubConnectionString, eventHubName, i)
-		if err != nil {
-			fmt.Println("Consumervnext::Error creating consumer client for partition", i, ":", err)
-			telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Failed to create new event hub instance"})
-			return
-		}
-		defer consumerClient.Close(context.Background())
-
-		consumerClients = append(consumerClients, consumerClient)
-	}
-
-	// Run your processing logic here using consumerClients...
-	fmt.Println("Consumervnext::Created", MaxPartitions, "consumer clients")
-	telemetry.TrackTrace("Consumervnext::Created "+strconv.Itoa(MaxPartitions)+" consumer clients", telemetry.Information, map[string]string{"PartitionCount": strconv.Itoa(MaxPartitions)})
-
-	// Handle termination signals for graceful shutdown
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-	<-stop
-
-	fmt.Println("Consumervnext::Shutting down...")
-	telemetry.TrackTrace("Consumervnext::Shutting down", telemetry.Information, nil)
-
-	// Close all consumer clients
-	for _, client := range consumerClients {
-		client.Close(context.Background())
-	}
-
-	fmt.Println("Consumervnext::Shutdown complete.")
-}
-
-func createConsumerClient(eventHubConnectionString, eventHubName string, partitionID int) (*azeventhubs.ConsumerClient, error) {
-	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, fmt.Sprintf("%d", partitionID), nil)
 	if err != nil {
-		fmt.Println("Consumervnext::Failed to create new consumer client for partition", partitionID, ":", err)
-		telemetry.TrackException(err, telemetry.Error, map[string]string{"Error": err.Error(), "Message": "Failed to create new event hub instance"})
+		fmt.Println("Error creating consumer client::", err)
 		return nil, err
 	}
 
-	telemetry.TrackTrace("Created consumer client for partition "+strconv.Itoa(partitionID), telemetry.Information, map[string]string{"PartitionID": fmt.Sprintf("%d", partitionID)})
-
 	return consumerClient, nil
+}
+
+func dispatchPartitionClients(processor *azeventhubs.Processor) {
+	for {
+		processorPartitionClient := processor.NextPartitionClient(context.TODO())
+
+		if processorPartitionClient == nil {
+			// Processor has stopped
+			break
+		}
+
+		go func() {
+			if err := processEventsForPartition(processorPartitionClient); err != nil {
+				fmt.Println("Error processing events for partition::", err)
+				panic(err)
+			}
+		}()
+	}
+}
+
+// processEventsForPartition shows the typical pattern for processing a partition.
+func processEventsForPartition(partitionClient *azeventhubs.ProcessorPartitionClient) error {
+	// 1. [BEGIN] Initialize any partition specific resources for your application.
+	// 2. [CONTINUOUS] Loop, calling ReceiveEvents() and UpdateCheckpoint().
+	// 3. [END] Cleanup any resources.
+
+	defer func() {
+		// 3/3 [END] Do cleanup here, like shutting down database clients
+		// or other resources used for processing this partition.
+		shutdownPartitionResources(partitionClient)
+	}()
+
+	// 1/3 [BEGIN] Initialize any partition specific resources for your application.
+	if err := initializePartitionResources(partitionClient.PartitionID()); err != nil {
+		return err
+	}
+
+	// 2/3 [CONTINUOUS] Receive events, checkpointing as needed using UpdateCheckpoint.
+	for {
+		// Wait up to a minute for 100 events, otherwise returns whatever we collected during that time.
+		receiveCtx, cancelReceive := context.WithTimeout(context.TODO(), time.Minute)
+		events, err := partitionClient.ReceiveEvents(receiveCtx, 100, nil)
+		cancelReceive()
+
+		if err != nil && !errors.Is(err, context.DeadlineExceeded) {
+			var eventHubError *azeventhubs.Error
+
+			if errors.As(err, &eventHubError) && eventHubError.Code == azeventhubs.ErrorCodeOwnershipLost {
+				return nil
+			}
+
+			return err
+		}
+
+		if len(events) == 0 {
+			continue
+		}
+
+		fmt.Printf("Received %d event(s)\n", len(events))
+
+		for _, event := range events {
+			fmt.Printf("Event received with body %v\n", event.Body)
+		}
+
+		// Updates the checkpoint with the latest event received. If processing needs to restart
+		// it will restart from this point, automatically.
+		if err := partitionClient.UpdateCheckpoint(context.TODO(), events[len(events)-1], nil); err != nil {
+			return err
+		}
+	}
+}
+
+func shutdownPartitionResources(partitionClient *azeventhubs.ProcessorPartitionClient) {
+	// Each PartitionClient holds onto an external resource and should be closed if you're
+	// not processing them anymore.
+	fmt.Printf("Shutting down resources for partition %s\n", partitionClient.PartitionID())
+	defer partitionClient.Close(context.TODO())
+}
+
+func initializePartitionResources(partitionID string) error {
+	// initialize things that might be partition specific, like a
+	// database connection.
+	fmt.Printf("Initializing resources for partition %s\n", partitionID)
+	return nil
 }
