19e6107ad73aa9ceddcddfbc9a89d275b8411ada
Pedro Rocha <pedrojrocha@outlook.com>
2024-04-01 09:12:27 +0200

    Update logging on consumervnext

--- consumervnext/consumervnext.go
+++ consumervnext/consumervnext.go
@@ -28,163 +28,101 @@ func main() {
 	//	partitionID := os.Getenv("EVENTHUB_PARTITION_ID")
 	eventHubName := "microtest-eventhub2"
 	partitionID := "0"
+	containerName := "partitionlease"
 
 	fmt.Println("Consumervnext::EventHubName::", eventHubName)
 	fmt.Println("Consumervnext::PartitionID::", partitionID)
 	fmt.Println("Consumervnext::EventHubConnectionString::", eventHubConnectionString)
 
-	storageConnectionString := os.Getenv("CHECKPOINTSTORE_STORAGE_CONNECTION_STRING")
-	storageContainerName := "partitionlease"
-
-	consumerClient, checkpointStore, err := createClients(eventHubConnectionString, eventHubName, storageConnectionString, storageContainerName)
+	// create a container client using a connection string and container name
+	checkClient, err := container.NewClientFromConnectionString(CHECKPOINTSTORE_STORAGE_CONNECTION_STRING, containerName, nil)
 
 	if err != nil {
-		fmt.Println("Error creating consumer client::", err)
+		fmt.Println("Consumervnext::Error creating container client::", err)
+		panic(err)
+	}
+
+	// create a checkpoint store that will be used by the event hub
+	checkpointStore, err := checkpoints.NewBlobStore(checkClient, nil)
+
+	if err != nil {
+		fmt.Println("Consumervnext::Error creating checkpoint store::", err)
+		panic(err)
+	}
+
+	// create a consumer client using a connection string to the namespace and the event hub
+	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
+
+	if err != nil {
+		fmt.Println("Consumervnext::Error creating consumer client::", err)
 		panic(err)
 	}
 
 	defer consumerClient.Close(context.TODO())
 
-	// Create the Processor
-	//
-	// The Processor handles load balancing with other Processor instances, running in separate
-	// processes or even on separate machines. Each one will use the checkpointStore to coordinate
-	// state and ownership, dynamically.
-	fmt.Println("Creating processor")
+	// create a processor to receive and process events
 	processor, err := azeventhubs.NewProcessor(consumerClient, checkpointStore, nil)
 
 	if err != nil {
-		fmt.Println("Error creating processor::", err)
+		fmt.Println("Consumervnext::Error creating processor::", err)
 		panic(err)
 	}
 
-	// Run in the background, launching goroutines to process each partition
-	go dispatchPartitionClients(processor)
+	//  for each partition in the event hub, create a partition client with processEvents as the function to process events
+	dispatchPartitionClients := func() {
+		for {
+			partitionClient := processor.NextPartitionClient(context.TODO())
 
-	// Run the load balancer. The dispatchPartitionClients goroutine (launched above)
-	// will receive and dispatch ProcessorPartitionClients as partitions are claimed.
-	//
-	// Stopping the processor is as simple as canceling the context that you passed
-	// in to Run.
+			if partitionClient == nil {
+				break
+			}
+
+			go func() {
+				if err := processEvents(partitionClient); err != nil {
+					panic(err)
+				}
+			}()
+		}
+	}
+
+	// run all partition clients
+	fmt.Println("Consumervnext::Starting partition clients")
+	go dispatchPartitionClients()
+
 	processorCtx, processorCancel := context.WithCancel(context.TODO())
 	defer processorCancel()
 
 	if err := processor.Run(processorCtx); err != nil {
-		fmt.Println("Error", err)
+		fmt.Println("Consumervnext::Error processor run::", err)
 		panic(err)
 	}
 }
 
-func createClients(eventHubConnectionString, eventHubName, storageConnectionString, storageContainerName string) (*azeventhubs.ConsumerClient, azeventhubs.CheckpointStore, error) {
-	// NOTE: the storageContainerName must exist before the checkpoint store can be used.
-	azBlobContainerClient, err := container.NewClientFromConnectionString(storageConnectionString, storageContainerName, nil)
-
-	if err != nil {
-		fmt.Println("Error creating blob container client::", err)
-		return nil, nil, err
-	}
-
-	checkpointStore, err := checkpoints.NewBlobStore(azBlobContainerClient, nil)
-
-	if err != nil {
-		fmt.Println("Error creating checkpoint store::", err)
-		return nil, nil, err
-	}
-
-	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
-
-	if err != nil {
-		fmt.Println("Error creating consumer client::", err)
-		return nil, err
-	}
-
-	return consumerClient, checkpointStore, nil
-}
-
-func dispatchPartitionClients(processor *azeventhubs.Processor) {
+func processEvents(partitionClient *azeventhubs.ProcessorPartitionClient) error {
+	defer closePartitionResources(partitionClient)
 	for {
-		processorPartitionClient := processor.NextPartitionClient(context.TODO())
-
-		if processorPartitionClient == nil {
-			// Processor has stopped
-			fmt.Println("Processor has stopped")
-			break
-		}
-
-		go func() {
-			if err := processEventsForPartition(processorPartitionClient); err != nil {
-				fmt.Println("Error processing events for partition::", err)
-				panic(err)
-			}
-		}()
-	}
-}
-
-// processEventsForPartition shows the typical pattern for processing a partition.
-func processEventsForPartition(partitionClient *azeventhubs.ProcessorPartitionClient) error {
-	// 1. [BEGIN] Initialize any partition specific resources for your application.
-	// 2. [CONTINUOUS] Loop, calling ReceiveEvents() and UpdateCheckpoint().
-	// 3. [END] Cleanup any resources.
-
-	defer func() {
-		// 3/3 [END] Do cleanup here, like shutting down database clients
-		// or other resources used for processing this partition.
-		fmt.Printf("Shutting down resources for partition %s\n", partitionClient.PartitionID())
-		shutdownPartitionResources(partitionClient)
-	}()
-
-	// 1/3 [BEGIN] Initialize any partition specific resources for your application.
-	if err := initializePartitionResources(partitionClient.PartitionID()); err != nil {
-		fmt.Printf("Error initializing resources for partition %s: %v\n", partitionClient.PartitionID(), err)
-		return err
-	}
-
-	// 2/3 [CONTINUOUS] Receive events, checkpointing as needed using UpdateCheckpoint.
-	for {
-		// Wait up to a minute for 100 events, otherwise returns whatever we collected during that time.
-		fmt.Printf("Receiving events for partition %s\n", partitionClient.PartitionID())
-		receiveCtx, cancelReceive := context.WithTimeout(context.TODO(), time.Minute)
+		receiveCtx, receiveCtxCancel := context.WithTimeout(context.TODO(), time.Minute)
 		events, err := partitionClient.ReceiveEvents(receiveCtx, 100, nil)
-		cancelReceive()
+		receiveCtxCancel()
 
 		if err != nil && !errors.Is(err, context.DeadlineExceeded) {
-			var eventHubError *azeventhubs.Error
-
-			if errors.As(err, &eventHubError) && eventHubError.Code == azeventhubs.ErrorCodeOwnershipLost {
-				return nil
-			}
-
 			return err
 		}
 
-		if len(events) == 0 {
-			continue
-		}
-
-		fmt.Printf("Received %d event(s)\n", len(events))
+		fmt.Printf("Processing %d event(s)\n", len(events))
 
 		for _, event := range events {
-			fmt.Printf("Event received with body %v\n", event.Body)
+			fmt.Printf("Event received with body %v\n", string(event.Body))
 		}
 
-		// Updates the checkpoint with the latest event received. If processing needs to restart
-		// it will restart from this point, automatically.
-		if err := partitionClient.UpdateCheckpoint(context.TODO(), events[len(events)-1], nil); err != nil {
-			return err
+		if len(events) != 0 {
+			if err := partitionClient.UpdateCheckpoint(context.TODO(), events[len(events)-1]); err != nil {
+				return err
+			}
 		}
 	}
 }
 
-func shutdownPartitionResources(partitionClient *azeventhubs.ProcessorPartitionClient) {
-	// Each PartitionClient holds onto an external resource and should be closed if you're
-	// not processing them anymore.
-	fmt.Printf("Shutting down resources for partition %s\n", partitionClient.PartitionID())
+func closePartitionResources(partitionClient *azeventhubs.ProcessorPartitionClient) {
 	defer partitionClient.Close(context.TODO())
-}
-
-func initializePartitionResources(partitionID string) error {
-	// initialize things that might be partition specific, like a
-	// database connection.
-	fmt.Printf("Initializing resources for partition %s\n", partitionID)
-	return nil
 }
